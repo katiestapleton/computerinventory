2627cb34a8fef1f464dcfb6a1b1d26e39e18da2a
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-01-20 13:08:37 -0600

    basic CRUD api

--- server.js
+++ server.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//request
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+//responding actions. send message  
+  res.send('Hello World! root')
+})
+
+//request on the hello endpoint
+app.get('/hello', (req, res) => {
+  res.send('get- Hello World!')
+})
+
+app.post('/hello', (req, res) => {
+  res.send('POST- Hello World!')
+})
+
+app.put('/hello', (req, res) => {
+  res.send('PUT- Hello World!')
+})
+
+app.delete('/hello', (req, res) => {
+  res.send('DELETE- Hello World!')
+})
+
+app.('/hello', (req, res) => {
+  res.send(' Hello World!')
 })
 
 app.listen(port, () => {
