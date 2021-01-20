e4ac2e55eebf60d8a95b9f8b0529e42951a48e38
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-01-20 11:15:52 -0600

    node setup

---
+++ server.js
@@ -0,0 +1,10 @@
+/*const http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  //write a response to the client
+  res.write('Hello from the Node server on your computer'); 
+  res.end(); //end the response
+}).listen(8081); //the server object listens on port 8081
+*/
+console.log("hello")
