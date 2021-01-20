b71080fb4f112f5390b821563227d92277306479
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-01-20 15:01:06 -0600

    MVP for app
    api for inventory and users.

--- express.js
+++ express.js
@@ -7,19 +7,60 @@ const port = 8089
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World from Express')
+
+// inventory API
+app.get('/inventory', (req, res) => {
+  res.send('GET the inventory')
 })
 
-app.get('/hello/:hello_id', (req, res) => {
-    res.json({"id": req.params.hello_id})
+app.get('/inventory/:component_id', (req, res) => {
+  res.send({'GET the component': req.params.component_id})
 })
 
-app.get('/object', (req, res) => {
-  const myObj = { "id": "1"}
-  res.json(myObj)
+app.post('/inventory', (req, res) => {
+  res.send('POST/add the inventory')
 })
 
+app.put('/inventory/:component_id', (req, res) => {
+  res.send({'UPDATE the component': req.params.component_id})
+})
+
+app.delete('/inventory/:component_id', (req, res) => {
+  res.send({'DELETE the component': req.params.component_id})
+})
+
+
+//users API
+
+app.get('/users', (req, res) => {
+  res.send('GET all users')
+})
+
+app.get('/users/:user_id', (req, res) => {
+  res.send({'GET user': req.params.user_id})
+})
+
+//login alt to get: send username/passwords/information without exposing 
+app.post('/users', (req, res) => {
+  res.send('POST/add user')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send(' with username and password')
+})
+
+
+app.put('/users/:user_id', (req, res) => {
+  res.send({'UPDATE user': req.params.user_id})
+})
+
+app.delete('/users/:user_id', (req, res) => {
+  res.send({'DELETE user': req.params.user_id})
+})
+
+
+
+// listen on port
 app.listen(port, () => {
     console.log(`Hello World from Express server listening at http://localhost:${port}`)
   })
